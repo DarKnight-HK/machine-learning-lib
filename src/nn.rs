1714322ef3dc83ca093d42727b8585bd5b4c9b7c
DarKnight-HK <ha986523@gmail.com>
2024-10-12 16:09:45 +0500

    Refining the model

--- src/nn.rs
+++ src/nn.rs
@@ -1,100 +1,31 @@
-use rand::Rng;
-use std::ops::{Add, Mul};
+use crate::matrix::Matrix;
 
-#[derive(Debug, Clone)]
-pub struct Matrix {
-    pub rows: usize,
-    pub cols: usize,
-    pub data: Vec<Vec<f64>>,
+#[derive(Debug)]
+pub struct NN {
+    //inner layers = count
+    pub count: usize,
+    pub weights: Vec<Matrix>,
+    pub biases: Vec<Matrix>,
+    pub activations: Vec<Matrix>,
 }
 
-impl Matrix {
-    pub fn new(rows: usize, cols: usize) -> Self {
-        Self {
-            rows,
-            cols,
-            data: vec![vec![0.0; cols]; rows],
+impl NN {
+    pub fn new(architecture: &[usize]) -> Self {
+        let mut nn = NN {
+            count: architecture.len() - 1,
+            weights: vec![],
+            biases: vec![],
+            activations: vec![],
+        };
+        nn.activations.push(Matrix::new(1, architecture[0]));
+        for i in 1..architecture.len() {
+            nn.weights
+                .push(Matrix::new(nn.activations[i - 1].cols, architecture[i]));
+
+            nn.biases.push(Matrix::new(1, architecture[i]));
+
+            nn.activations.push(Matrix::new(1, architecture[i]));
         }
-    }
-
-    pub fn print(&self, name: &str) {
-        println!("{} = [", name);
-        for row in self.data.iter() {
-            for value in row.iter() {
-                print!("    {:.2} ", value);
-            }
-            println!();
-        }
-        println!("]");
-    }
-
-    pub fn fill(&mut self, value: f64) {
-        for row in self.data.iter_mut() {
-            for mat_value in row.iter_mut() {
-                *mat_value = value;
-            }
-        }
-    }
-
-    pub fn fill_rand(&mut self, mut low: f64, mut high: f64) {
-        if low > high {
-            std::mem::swap(&mut low, &mut high);
-        }
-        let mut rng = rand::thread_rng();
-        for row in self.data.iter_mut() {
-            for value in row.iter_mut() {
-                *value = rng.gen_range(low..high);
-            }
-        }
-    }
-
-    pub fn sigmoid(&mut self) {
-        for i in 0..self.rows {
-            for j in 0..self.cols {
-                let value = self.data[i][j];
-                let sigmoid_value = 1.0 / (1.0 + (-value).exp());
-                self.data[i][j] = sigmoid_value;
-            }
-        }
-    }
-}
-
-impl<'a> Add<&'a Matrix> for &'a Matrix {
-    type Output = Result<Matrix, &'static str>;
-
-    fn add(self, other: Self) -> Self::Output {
-        if self.rows != other.rows || self.cols != other.cols {
-            return Err("Dimensions must be the same");
-        }
-
-        let mut new_mat = Matrix::new(self.rows, self.cols);
-        for i in 0..self.rows {
-            for j in 0..self.cols {
-                new_mat.data[i][j] = self.data[i][j] + other.data[i][j];
-            }
-        }
-
-        Ok(new_mat)
-    }
-}
-
-impl<'a> Mul<&'a Matrix> for &'a Matrix {
-    type Output = Result<Matrix, &'static str>;
-
-    fn mul(self, other: Self) -> Self::Output {
-        if self.cols != other.rows {
-            return Err("Inner dimensions must be same");
-        }
-
-        let mut new_mat = Matrix::new(self.rows, other.cols);
-        for i in 0..self.rows {
-            for j in 0..other.cols {
-                new_mat.data[i][j] = 0.0;
-                for k in 0..self.cols {
-                    new_mat.data[i][j] += self.data[i][k] * other.data[k][j];
-                }
-            }
-        }
-        Ok(new_mat)
+        nn
     }
 }
